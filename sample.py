84a72d7fefe7da7d297bb1aa5757e3924fca7a2e
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-12 18:45:48 +0530

    add code file12

--- sample.py
+++ sample.py
@@ -14,7 +14,17 @@ def main(args=None):
     onnx_path = '/kaggle/working/fp32_updated.onnx'
     torch.onnx.export(retinanet,example_input,onnx_path,opset_version=15)
     print('export completed')
-
+        # model being run
+    ort_session = onnxruntime.InferenceSession('/kaggle/working/fp32_updated.onnx')
+    ort_inputs = {'input.1': None}
+    dataset_val = CocoDataset('/kaggle/input/coco-2017-dataset/coco2017', set_name='val2017',
+                              transform=transforms.Compose([Normalizer(), Resizer_const()])) 
+    data = dataset_val[0]
+    inputs = data['img'].permute(2, 0, 1).float().unsqueeze(dim=0)
+    ort_inputs['input.1'] = inputs.cpu().numpy()
+    ort_outs = ort_session.run(None, ort_inputs)
+    anchors,classification = ort_outs[0], ort_outs[1], ort_outs[2] 
+    print(classification.shape,anchors.shape)
 if __name__ == '__main__':
     main()
  
