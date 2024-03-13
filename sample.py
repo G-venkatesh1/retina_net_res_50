0f5ff0c2b3238e731a5c95e63701cfddf7c81498
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-13 14:40:31 +0530

    add code files2

--- sample.py
+++ sample.py
@@ -19,7 +19,8 @@ def main(args=None):
     # ort_session = onnxruntime.InferenceSession('/kaggle/working/fp32_updated.onnx')
     # ort_inputs = {'input.1': None}
     dataset_val = CocoDataset('/kaggle/input/coco-2017-dataset/coco2017', set_name='val2017',
-                              transform=transforms.Compose([Normalizer(), Resizer_const()])) 
+                              transform=transforms.Compose([Normalizer(), Resizer_const()]))
+    print(dataset_val) 
     # data = dataset_val[0]
     # inputs = data['img'].permute(2, 0, 1).float().unsqueeze(dim=0)
     # ort_inputs['input.1'] = inputs.cpu().numpy()
@@ -32,22 +33,22 @@ def main(args=None):
     # ort_outs = ort_session.run(None, ort_inputs)
     # anchors,classification = ort_outs[0], ort_outs[1]
     # print(classification.shape,anchors.shape)
-    onnx_fp_32_path ='/kaggle/working/retina_net_res_50/fp_32_preprocess.onnx'
+    # onnx_fp_32_path ='/kaggle/working/retina_net_res_50/fp_32_preprocess.onnx'
     # onnx_fp_16_path = '/kaggle/working/onnx_fp_16.onnx'
     # onnx_32_model = onnx.load(onnx_fp_32_path)
     # onnx_16_model = float16.convert_float_to_float16(onnx_32_model,min_positive_val=1e-7,max_finite_val=1e4)
     # onnx.save(onnx_16_model,onnx_fp_16_path)
-    int8_onnx_path ='/kaggle/working'
+    # int8_onnx_path ='/kaggle/working'
     # ort.quantization.shape_inference.quant_pre_process(onnx_fp_32_path, int8_onnx_path)
-    module = quantise.OnnxStaticQuantization()
-    module.fp32_onnx_path = onnx_fp_32_path
-    module.quantization(
-        fp32_onnx_path=onnx_fp_32_path,
-        future_int8_onnx_path=int8_onnx_path,
-        calib_method="Percentile",
-        calibration_loader=dataset_val,
-        sample=100
-    )
+    # module = quantise.OnnxStaticQuantization()
+    # module.fp32_onnx_path = onnx_fp_32_path
+    # module.quantization(
+    #     fp32_onnx_path=onnx_fp_32_path,
+    #     future_int8_onnx_path=int8_onnx_path,
+    #     calib_method="Percentile",
+    #     calibration_loader=dataset_val,
+    #     sample=100
+    # )
     
     
     
