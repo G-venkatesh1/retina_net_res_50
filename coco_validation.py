79533f090cf53ab4e3033e5cce9cd6700134297d
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-14 09:58:05 +0530

    add code files2

--- coco_validation.py
+++ coco_validation.py
@@ -41,7 +41,7 @@ def main(args=None):
     retinanet.training = False
     retinanet.eval()
     retinanet.module.freeze_bn()
-    ort_session = onnxruntime.InferenceSession('/kaggle/working/int_8.onnx')
+    ort_session = onnxruntime.InferenceSession('/kaggle/working/int_8_mm_w16a16.onnx')
     coco_eval.evaluate_coco(dataset_val,ort_session)
  
 
